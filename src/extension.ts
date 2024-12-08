8bb3302b4fe3e10850e5788be3598019361ab0e4
AkiEga <akira.ega@gmail.com>
2024-12-08 18:11:50 +0900

    support to touch drawio svg file

--- src/extension.ts
+++ src/extension.ts
@@ -6,90 +6,133 @@ import fs from 'fs';
 import os from 'os';
 import ChildProcess from 'child_process';
 
+class ClipboardImage {
+	img: string = "";
+	width: number = 0;
+	height: number = 0;
+}
+
 // read clipboard image data as Uint8Array
-function readClipboardImage():Uint8Array {
-	let imgData:Uint8Array = new Uint8Array(0);
+function readClipboardImage(): ClipboardImage {
+	let clipboardImage: ClipboardImage = new ClipboardImage();
 	try {
 		// exec command to get clipboard image data with powershell		
-		let base64Str = "";
 		if (os.platform() === "win32") {
-			let pwshcmd = `Add-Type -AssemblyName System.Windows.Forms;
-			$image = [System.Windows.Forms.Clipboard]::GetImage()
-			$ms = New-Object -TypeName System.IO.MemoryStream;
-			$image.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png);
-			$encoded = [System.Convert]::ToBase64String($ms.GetBuffer());
-			Write-Host $encoded`;
+			// TODO: implement to get clipboard image data with powershell
+			// let pwshcmd = `
+			// Add-Type -AssemblyName System.Windows.Forms;
+			// $obj = [System.Windows.Forms.Clipboard]::GetDataObject();
+			// if ($true -eq $obj.ContainsImage()) {
+			// 	$image = [System.Windows.Forms.Clipboard]::GetImage()
+			// 	$ms = New-Object -TypeName System.IO.MemoryStream;
+			// 	$image.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png);
+			// 	$encoded = [System.Convert]::ToBase64String($ms.GetBuffer());
+			// 	$json = @{"img"=$encoded;"width"=$image.Width; "height"=$image.Height}
+			// } else {
+			// 	$json = @{"img"="";"width"=0; "height"=0}
+			// }
+			// $json | ConvertTo-Json`;
+			let pwshcmd = `
+			Add-Type -AssemblyName System.Windows.Forms;
+			$obj = [System.Windows.Forms.Clipboard]::GetDataObject();
+			if ($true -eq $obj.ContainsImage()) {
+				$image = [System.Windows.Forms.Clipboard]::GetImage()
+				$json = @{"img"="It will be insert base64 str";"width"=$image.Width; "height"=$image.Height}
+			} else {
+				$json = @{"img"="";"width"=0; "height"=0}
+			}
+			$json | ConvertTo-Json`;
 			// exec command to get clipboard image data with powershell
-			base64Str = ChildProcess.execSync(pwshcmd, {shell: "pwsh.exe", encoding: "utf8"});
+			let clipboardImageJson = ChildProcess.execSync(pwshcmd, {shell: "pwsh.exe", encoding: "utf8"});
+			// convert json string to ClipboardImage object
+			clipboardImage = JSON.parse(clipboardImageJson);
 		} else {
 			vscode.window.showInformationMessage(`This extension is not support this platform: ${os.platform()}!`);
 		}
-
-		// convert base64 string to Uint8Array
-		imgData = new Uint8Array(Buffer.from(base64Str, "base64"));
 	} catch (error) {
 		console.error("[Error] failed to read clipboard image data!");
 	}
-	return imgData;	
+	return clipboardImage;
 }
 
-async function touchSvgFile() {
+function createDrawioSvgFile(tarUri: vscode.Uri, ci: ClipboardImage) {		
+	let newFileContentStr = "";
+	// TODO: implement to write to drawio.svg file
+	// // read base drawio.svg file content
+	// // let newFileContentStr = templateDrawioSvg + `
+	// // let newFileContentStr = `<svg xmlns="http://www.w3.org/2000/svg"
+	// // 	xmlns:xlink="http://www.w3.org/1999/xlink">
+	// // 	<image x="0" y="0" width="${ci.width}" height="${ci.height}" xlink:href="data:image/png;base64,${ci.img}" />
+	// // </svg>`;
+	
+	// convert string to Uint8Array
+	let newFileContent = new TextEncoder().encode(newFileContentStr);
+	vscode.workspace.fs.writeFile(tarUri, newFileContent);	
+}
+
+async function quickPasteAsDrawioSvg(editor: vscode.TextEditor, ws: vscode.WorkspaceFolder) {
 	// The code you place here will be executed every time your command is executed
 	// Display a message box to the user
-	let editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
-	let ws = vscode.workspace.getWorkspaceFolder(editor?.document.uri);
-	if (!ws) {
-		return;
-	}
-
-	// Touch(=create=write) an drawio.svg file
-	let conf = vscode.workspace.getConfiguration("quick-paste-as-drawio-svg");
-	// get imgDir default value from package.json
-	let imgDir:string = conf.get("img-dir")??"";
-	let newFileUri = ws.uri;
-	let newFileContent = new Uint8Array();
-	// get image data from clipboard
-
+	let pasteMode = "image";
+	let newFileUri = vscode.Uri.file("");
+	let insertText = "";
 	try {
-		// Check if new file is already exist
-		for (let id = 0;; id++) {
-			let prefix = conf.get("img-file-prefix")??"";
-			let newFileName = `${prefix}_${id}.drawio.svg`;
-			newFileUri = vscode.Uri.joinPath(ws.uri, imgDir, newFileName);
-			// let ret = await vscode.workspace.findFiles(newFileUri.fsPath);
-			if (!fs.existsSync(newFileUri.fsPath)) {
-				// passed
-				break;
-			}
+		// get image data from clipboard
+		let ci = readClipboardImage();
+		if (ci.img === "") {
+			pasteMode = "text";
+			// get text data from clipboard
+			insertText = await vscode.env.clipboard.readText();
+		} else {
+			// Touch(=create=write) an drawio.svg file
+			// Check if new file is already exist
+			let conf = vscode.workspace.getConfiguration("quick-paste-as-drawio-svg");
+			let imgDir:string = conf.get("img-dir")??"";
+			let prefix:string = conf.get("img-file-prefix") ?? "";
+			newFileUri = GenNewFileUri(ws, imgDir, prefix);		
+			createDrawioSvgFile(newFileUri, ci);
+			
+			vscode.window.showInformationMessage(`write new file: ${newFileUri} from quick-paste-as-drawio-svg!`);
 		}
-		vscode.workspace.fs.writeFile(newFileUri, newFileContent);	
 	} catch (error) {
 		console.error("[Error] failed to write a drawio.svg file from quick-paste-as-drawio-svg extension!");
 	}
-	vscode.window.showInformationMessage(`write new file: ${newFileUri} from quick-paste-as-drawio-svg!`);
 
 	// append a drawio.svg markdown link to active editor
 	editor.edit((e)=>{
 		if (ws && editor) {
-			// make relative path of a *.drawio.svg file 
-			let parentDir = path.dirname(editor.document.uri.fsPath);
-			let newFilePath = newFileUri.fsPath;
+			if (pasteMode === "image") {
+				// make relative path of a *.drawio.svg file 
+				let parentDir = path.dirname(editor.document.uri.fsPath);
+				let newFilePath = newFileUri.fsPath;
 
-			// Convert to relative path
-			let relativePath = "";
-			if (os.platform() === "win32"){
-				relativePath = path.win32.relative(parentDir, newFilePath).replace(/\\/g, "/");
-			} else {
-				relativePath = path.relative(parentDir, newFilePath);
+				// Convert to relative path
+				let relativePath = "";
+				if (os.platform() === "win32"){
+					relativePath = path.win32.relative(parentDir, newFilePath).replace(/\\/g, "/");
+				} else {
+					relativePath = path.relative(parentDir, newFilePath);
+				}
+				insertText = `![](${relativePath})`;
 			}
-			
+			// insert text to active editor
 			let currentPos = editor.selection.end;
-			e.insert(currentPos, `![](${relativePath})`);
+			e.insert(currentPos, insertText);
 		}
 	});
+}
+
+function GenNewFileUri(ws: vscode.WorkspaceFolder, imgDir: string = "", prefix: string = ""): vscode.Uri {
+	let newFileUri: vscode.Uri;
+	for (let id = 0; ; id++) {
+		let newFileName = `${prefix}_${id}.drawio.svg`;
+		newFileUri = vscode.Uri.joinPath(ws.uri, imgDir, newFileName);
+		if (!fs.existsSync(newFileUri.fsPath)) {
+			// passed
+			break;
+		}
+	}
+	return newFileUri;
 }
 
 // This method is called when your extension is activated
@@ -103,7 +146,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quick-paste-as-drawio-svg.create-new-drawio-svg', touchSvgFile);
+	let disposable = vscode.commands.registerCommand('quick-paste-as-drawio-svg.create-new-drawio-svg', async () => {
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		let ws = vscode.workspace.getWorkspaceFolder(editor?.document.uri);
+		if (!ws) {
+			return;
+		}
+		quickPasteAsDrawioSvg(editor, ws);
+	});
 
 	context.subscriptions.push(disposable);
 }
