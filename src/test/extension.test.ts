3208d7a5293e38a39bd4d582bafafb3150a35ebc
AkiEga <akira.ega@gmail.com>
2024-12-13 00:23:44 +0900

    append vscode-test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,13 +3,22 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as myExtension from '../extension';
+import * as vscodeTest from 'vscode-test';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+	test('Sample test', async () => {
+		// open folder
+		await vscodeTest.runTests({
+			extensionDevelopmentPath: __dirname,
+			extensionTestsPath: __dirname,
+			launchArgs: [__dirname],
+		}).then(()=>{
+			myExtension.resolveImgDir("${workspaceFolder}/img", undefined).then((ret)=>{
+				let expected = __dirname + "/img";
+				assert.strictEqual(ret, expected);
+			});
+		});
 });
+
